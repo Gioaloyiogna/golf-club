85479ebe08121370813111c1522d88f0babdd291
GioAloyiogna <gio.aloyiogna@gmail.com>
2023-05-09 16:58:08 +0000

    new changes

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -1,47 +1,3 @@
-// import axios from 'axios'
-// import {AuthModel, UserModel} from './_models'
-// import { API_URL } from '../../../urls'
-
-// // const API_URL = process.env.REACT_APP_API_URL
-
-// export const GET_USER_BY_ACCESSTOKEN_URL = `${API_URL}/verify_token`
-// export const LOGIN_URL = `${API_URL}/auth`
-// export const REGISTER_URL = `${API_URL}/register`
-// export const REQUEST_PASSWORD_URL = `${API_URL}/forgot_password`
-
-// // Server should return AuthModel
-// export function login(values:any) {
-//   console.log('values', values);
-  
-//   return axios.post<AuthModel>(LOGIN_URL, 
-//     values)
-// }
-
-// // Server should return AuthModel
-// export function register(
-//   email: string,
-//   firstname: string,
-//   lastname: string,
-//   password: string,
-//   password_confirmation: string
-// ) {
-//   return axios.post(REGISTER_URL, {
-//     email,
-//     first_name: firstname,
-//     last_name: lastname,
-//     password,
-//     password_confirmation,
-//   })
-// }
-
-// // Server should return object => { result: boolean } (Is Email in DB)
-// export function requestPassword(email: string) {
-//   return axios.post<{result: boolean}>(REQUEST_PASSWORD_URL, {
-//     email,
-//   })
-// }
-
-
 
 
 import axios from 'axios'
@@ -97,8 +53,3 @@ export function requestPassword(email: string) {
   })
 }
 
-// export function getUserByToken(token: string) {
-//   return axios.post<UserModel>(GET_USER_BY_ACCESSTOKEN_URL, {
-//     api_token: token,
-//   })
-// }
